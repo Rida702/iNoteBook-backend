22e29a4f6b008286b883ad9277917e1e89f26e19
Rida702 <ai22c0rida@gmail.com>
2024-09-27 15:38:02 +0500

    Create login

--- routes/auth.js
+++ routes/auth.js
@@ -42,8 +42,41 @@ router.post('/createuser', [
     }
     catch (error) {
         console.error(error.message);
-        res.status(500).send("Some Error Occured");
+        res.status(500).send("Internal Server error");
     }
 })
 
+//Authenticate a user using: POST "/api/auth/login"
+router.post('/login', [
+    body('email', "Enter a valid email").isEmail(),
+    body('password', "Password cannot be blank").exists()
+], async (req, res) => {
+    //If there are errors return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {email, password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if (!user){
+            res.status(400).json({error: "Please try to login with correct credentials"});
+        }
+        const passCompare = await bcrypt.compare(password, user.password)
+        if (!passCompare){
+            res.status(400).json({error: "Please try to login with correct credentials"});
+        }
+        const payload = {
+            user:{
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        res.json({authtoken});
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server error");
+    }
+})
 module.exports = router;
