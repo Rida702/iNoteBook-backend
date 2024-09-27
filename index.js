3e0d63641baa0cb6ad2e0643045a55a6006275a9
Rida702 <ai22c0rida@gmail.com>
2024-09-27 12:40:15 +0500

    create schema and enpoints for user and notes

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello RIDA!')
-})
+//Available routes
+// app.get('/', (req, res) => {
+//   res.send('Hello RIDA!')
+// })
+//Firs the router will search in index.js and then in the file mentioned in ./routes/auth
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
